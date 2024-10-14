4656991a4c63913920d0c5e7816ca6af4a47c67f
wisdomgo <1246045735@qq.com>
2024-10-15 00:33:17 +0800

    finish 3.rs

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -5,8 +5,16 @@
 */
 // I AM NOT DONE
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn sort<T:Ord>(array: &mut [T])
+{
+    let len = array.len();
+    for i in 0..len {
+        for j in 0..len - 1 -i {
+            if array[j] > array[j + 1] {
+                array.swap(j, j + 1);
+            }
+        }
+    }
 }
 #[cfg(test)]
 mod tests {
